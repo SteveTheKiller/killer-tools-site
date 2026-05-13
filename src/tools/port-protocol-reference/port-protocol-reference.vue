f89409097768cb95b34428075599233be768d0ec
Steve Riley <steve@thekiller.net>
2026-05-12 17:08:32 -0700

    remove line-clamp truncation from port-protocol-reference

--- src/tools/port-protocol-reference/port-protocol-reference.vue
+++ src/tools/port-protocol-reference/port-protocol-reference.vue
@@ -272,11 +272,6 @@ const totalVisible = computed(() => filtered.value.reduce((sum, c) => sum + c.po
   font-size: 0.75rem;
   color: rgba(255, 255, 255, 0.55);
   line-height: 1.5;
-  display: -webkit-box;
-  -webkit-line-clamp: 3;
-  line-clamp: 3;
-  -webkit-box-orient: vertical;
-  overflow: hidden;
   margin-top: 2px;
   padding-top: 4px;
   border-top: 1px solid rgba(255, 255, 255, 0.06);
