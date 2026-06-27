270e90d2dc8fba85d071434be05306ac8df559a7
Steve Riley <steve@thekiller.net>
2026-06-27 05:38:23 -0700

    update killerPDF.net redirect

--- src/tools/killer-pdf/killer-pdf.vue
+++ src/tools/killer-pdf/killer-pdf.vue
@@ -5,7 +5,7 @@ import { useRouter } from 'vue-router';
 const router = useRouter();
 
 onMounted(() => {
-  window.open('https://pdf.killertools.net', '_blank', 'noopener');
+  window.open('https://killerpdf.net', '_blank', 'noopener');
   router.replace('/');
 });
 </script>
